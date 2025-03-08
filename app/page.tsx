451e9e8cb99191cccce9f98d9203d0cb68444a3b
chandan <dbgchandan441@gmail.com>
2025-03-08 16:27:31 +0530

    setUp

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div>
-      Hello Developer!
-    </div>
+    <>
+      <div>Hello Developer!</div>
+      <Button>Subscribe</Button>
+    </>
   );
 }
